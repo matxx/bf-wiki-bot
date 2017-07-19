3e5d695b63999cb46d2d0b7949c90e2332dd52c0
matxx <matxx.webmaster@gmail.com>
2017-07-19 07:41:31 +0200

    gem init

---
+++ lib/bf/wiki/bot.rb
@@ -0,0 +1,9 @@
+require "bf/wiki/bot/version"
+
+module BF
+  module Wiki
+    module Bot
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/bf/wiki/bot/version.rb
@@ -0,0 +1,7 @@
+module BF
+  module Wiki
+    module Bot
+      VERSION = "0.1.0"
+    end
+  end
+end
